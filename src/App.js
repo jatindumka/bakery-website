6355ab5708869944565df04bc7c6a260b3faac2a
jatin <jatindumka@gmail.com>
2024-03-11 17:48:07 +0530

    link issue fix

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='me-auto justify-content-end w-100'>
-              <Nav.Link href='/' className='active text-uppercase'>Home</Nav.Link>
-              <Nav.Link href='/menu' className='text-uppercase'>Menu</Nav.Link>
-              <Nav.Link href='/about' className='text-uppercase'>About</Nav.Link>
-              <Nav.Link href='/contact' className='text-uppercase'>Contact</Nav.Link>
+              <Link href='/' className='active text-uppercase'>Home</Link>
+              <Link href='/menu' className='text-uppercase'>Menu</Link>
+              <Link href='/about' className='text-uppercase'>About</Link>
+              <Link href='/contact' className='text-uppercase'>Contact</Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
